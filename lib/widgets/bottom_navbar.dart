9bfb5a127f3ac1635c9afe740f79172ceedb6d99
Kariuki Joseph <kariuki.joseph121@gmail.com>
2024-01-18 17:11:32 +0300

    bottom navigation bar

---
+++ lib/widgets/bottom_navbar.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FarmerBottomNavigationBar extends StatelessWidget {
+  const FarmerBottomNavigationBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+
+  void openPage(index) {}
+}
