87ad94bda6fb1567009ef6bedcfc8eaee7909c11
Kariuki Joseph <kariuki.joseph121@gmail.com>
2024-01-18 07:07:48 +0300

    app routes

---
+++ lib/routes/app_routs.dart
@@ -0,0 +1,17 @@
+import 'package:get/get_navigation/src/routes/get_route.dart';
+
+class AppRoute {
+  static const homePageDairy = "/HomePageDairy";
+  static const homePageFarmer = "/HomePageFarmer";
+  static const chooseAccount = "/ChooseAcount";
+  static const register = "/Register";
+  static const login = "/Login";
+
+  // static List<GetPage> routes = [
+  //   GetPage(name: homePageDairy, page: () => const HomePageDairy()),
+  //   GetPage(name: homePageFarmer, page: () => const HomePageFarmer()),
+  //   GetPage(name: chooseAccount, page: () => const ChooseAccount()),
+  //   GetPage(name: register, page: () => const Register()),
+  //   GetPage(name: login, page: () => const Login()),
+  // ];
+}
