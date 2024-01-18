f4aa04930b5aeb38922d1851fa33ad7f4de2aaa9
Kariuki Joseph <kariuki.joseph121@gmail.com>
2024-01-18 19:35:46 +0300

    add farmer home tab with bottom navbar

---
+++ lib/screens/home/farmer_home_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:wanda_dairy/screens/home/tabs/farmer_home_tab.dart';
+import 'package:wanda_dairy/screens/home/tabs/farmer_invoice_tab.dart';
+
+class FarmerHomePage extends StatefulWidget {
+  const FarmerHomePage({super.key});
+
+  @override
+  State<FarmerHomePage> createState() => _FarmerHomePageState();
+}
+
+class _FarmerHomePageState extends State<FarmerHomePage> {
+  int _currentIndex = 0;
+  final PageController _pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: PageView(
+            controller: _pageController,
+            onPageChanged: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            children: const [
+              FarmerHomeTab(),
+              FarmerInvoiceTab(),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.add_card), label: "Invoices")
+          ],
+          onTap: (index) {
+            _pageController.animateToPage(
+              index,
+              duration: const Duration(milliseconds: 100),
+              curve: Curves.easeIn,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
