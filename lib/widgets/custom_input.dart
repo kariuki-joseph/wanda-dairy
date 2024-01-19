846cb17dfd6eb761e4593ae6b73c90e77c0be5c8
Kariuki Joseph <kariuki.joseph121@gmail.com>
2024-01-19 07:46:23 +0300

    expose onchanged function

--- lib/widgets/custom_input.dart
+++ lib/widgets/custom_input.dart
@@ -5,19 +5,25 @@ class CustomTextFormField extends StatelessWidget {
   final String? hintText;
   final bool? obscureText;
   final TextInputType? keyboardType;
-  const CustomTextFormField(
-      {Key? key,
-      required this.labelText,
-      this.hintText,
-      this.keyboardType,
-      this.obscureText})
-      : super(key: key);
+  final TextEditingController? controller;
+  final Function(String)? onChanged;
+  const CustomTextFormField({
+    Key? key,
+    required this.labelText,
+    this.hintText,
+    this.keyboardType,
+    this.controller,
+    this.obscureText,
+    this.onChanged,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       keyboardType: keyboardType,
       obscureText: obscureText ?? false,
+      controller: controller,
+      onChanged: onChanged,
       decoration: InputDecoration(
         labelText: labelText,
         hintText: hintText,
