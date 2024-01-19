7b4889f40347873abb9082cc8829a7cb34ffe4ba
Kariuki Joseph <kariuki.joseph121@gmail.com>
2024-01-19 07:46:50 +0300

    add more routes

--- lib/routes/app_routs.dart
+++ lib/routes/app_routs.dart
@@ -1,17 +1,22 @@
 import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:wanda_dairy/screens/choose_account.dart';
+import 'package:wanda_dairy/screens/home/dairy_home_page.dart';
+import 'package:wanda_dairy/screens/home/farmer_home_page.dart';
+import 'package:wanda_dairy/screens/login/login.dart';
+import 'package:wanda_dairy/screens/register/register.dart';
 
 class AppRoute {
-  static const homePageDairy = "/HomePageDairy";
-  static const homePageFarmer = "/HomePageFarmer";
   static const chooseAccount = "/ChooseAcount";
+  static const dairyHomePage = "/DairyHomePage";
+  static const farmerHomePage = "/FarmerHomePage";
   static const register = "/Register";
   static const login = "/Login";
 
-  // static List<GetPage> routes = [
-  //   GetPage(name: homePageDairy, page: () => const HomePageDairy()),
-  //   GetPage(name: homePageFarmer, page: () => const HomePageFarmer()),
-  //   GetPage(name: chooseAccount, page: () => const ChooseAccount()),
-  //   GetPage(name: register, page: () => const Register()),
-  //   GetPage(name: login, page: () => const Login()),
-  // ];
+  static List<GetPage> routes = [
+    GetPage(name: chooseAccount, page: () => const ChooseAccount()),
+    GetPage(name: dairyHomePage, page: () => const DairyHomePage()),
+    GetPage(name: farmerHomePage, page: () => const FarmerHomePage()),
+    GetPage(name: register, page: () => const Register()),
+    GetPage(name: login, page: () => const Login()),
+  ];
 }
